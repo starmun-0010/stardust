285fa5142edd2d3dcb61387cb9dbae9410758053
starmun <abdemuqsit@hotmail.com>
2022-03-18 19:12:32 +0500

    Added ores to data gen

--- common/src/main/java/xyz/starmun/stardust/data/generators/OreConfigGenerator.java
+++ common/src/main/java/xyz/starmun/stardust/data/generators/OreConfigGenerator.java
@@ -17,7 +17,456 @@ public class OreConfigGenerator extends DataProviderBase {
     public void run(HashCache hashCache) {
         List<Ore> ores = new ArrayList<Ore>(){{
             add(Ore.builder
+                    .setId("aluminum")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("apatite")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("arcane")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("bitumen")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("brass")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("bronze")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("cast_iron")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("certus_quartz")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("charged_certus_quartz")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("cinnabar")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("cobalt")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("coke")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("constantan")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("copper")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("dimensional")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("electrum")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("enderium")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("fluix")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
+                    .setId("fluorite")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("invar")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("iridium")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+            add(Ore.builder
                     .setId("iron")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("lead")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("lumium")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("nickel")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("osmium")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("peridot")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("potassium")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("ruby")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("saphire")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("signalum")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("silver")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("steel")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("sulfur")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("tin")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("uranium")
+                    .setColors(new ArrayList<String>(){{
+                        add("");
+                    }})
+                    .addItem(Ore.Item.builder
+                            .setNameSuffix("gear")
+                            .setColors(new ArrayList<String>(){{
+                                add("");
+                            }})
+                            .build())
+                    .build());
+
+            add(Ore.builder
+                    .setId("zinc")
                     .setColors(new ArrayList<String>(){{
                         add("");
                     }})
